d61695e593f6b6ca664a6aca6b93ff22caf6dd98
Faruq <faruqdeveloper@gmail.com>
2022-04-24 02:20:07 +0600

    page tab script

--- PagerTabStrip/ViewController.swift
+++ PagerTabStrip/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var view2: UIView!
+    @IBOutlet weak var view1: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func secondtabclicked(_ sender: Any) {
+        view1.isHidden = false
+        view2.isHidden = true
+        
+    }
+    @IBAction func firstzTabClick(_ sender: Any) {
+        view1.isHidden = true
+        view2.isHidden = false
+        
+    }
 }
 
